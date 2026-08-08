517e46996f012ca081ee499d53adf6ace74b19ee
Michael (Parker) Parker <parkervcp@gmail.com>
2026-08-08 13:39:58 -0400

    remove old ufs code

--- config/config_openat_darwin.go
+++
@@ -1,9 +0,0 @@
-//go:build darwin
-
-package config
-
-// UseOpenat2 always returns false on Darwin as the openat2 syscall is
-// Linux-specific (kernel 5.6+).
-func UseOpenat2() bool {
-	return false
-}
--- internal/ufs/file_linux.go
+++
@@ -1,7 +0,0 @@
-//go:build linux
-
-package ufs
-
-import "golang.org/x/sys/unix"
-
-const O_LARGEFILE = unix.O_LARGEFILE
